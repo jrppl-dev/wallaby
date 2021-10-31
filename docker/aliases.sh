78c05a4eb7f224d96ffc9c5af6686b9f378d2e11
jrppl <jrppl.dev@gmail.com>
2021-10-31 18:22:57 +0000

    wallaby-1 Set up local dev env
    Signed-off-by: jrppl <jrppl.dev@gmail.com>

--- docker/aliases.sh
+++ docker/aliases.sh
@@ -1,4 +1,13 @@
-#! /bin/bash
+alias @source="source ~/.zshrc"
+alias @alias="vim ~/.bash_aliases && source ~/.zshrc"
+# PhpStorm IDEA Tools
+alias d:up="docker-compose up"
+alias d:down="docker-compose down"
+alias d:build="docker-compose build"
+alias d:workspace="docker-compose exec --user=laradock workspace zsh"
+alias d:push="docker-compose push"
+alias d:pull="docker-compose pull"
+alias d:ps="docker-compose ps"
 
 # Colors used for status updates
 ESC_SEQ="\x1b["
@@ -12,11 +21,11 @@ COL_CYAN=$ESC_SEQ"36;01m"
 
 # Detect which `ls` flavor is in use
 if ls --color > /dev/null 2>&1; then # GNU `ls`
-	colorflag="--color"
-	export LS_COLORS='no=00:fi=00:di=01;31:ln=01;36:pi=40;33:so=01;35:do=01;35:bd=40;33;01:cd=40;33;01:or=40;31;01:ex=01;32:*.tar=01;31:*.tgz=01;31:*.arj=01;31:*.taz=01;31:*.lzh=01;31:*.zip=01;31:*.z=01;31:*.Z=01;31:*.gz=01;31:*.bz2=01;31:*.deb=01;31:*.rpm=01;31:*.jar=01;31:*.jpg=01;35:*.jpeg=01;35:*.gif=01;35:*.bmp=01;35:*.pbm=01;35:*.pgm=01;35:*.ppm=01;35:*.tga=01;35:*.xbm=01;35:*.xpm=01;35:*.tif=01;35:*.tiff=01;35:*.png=01;35:*.mov=01;35:*.mpg=01;35:*.mpeg=01;35:*.avi=01;35:*.fli=01;35:*.gl=01;35:*.dl=01;35:*.xcf=01;35:*.xwd=01;35:*.ogg=01;35:*.mp3=01;35:*.wav=01;35:'
-else # macOS `ls`
-	colorflag="-G"
-	export LSCOLORS='BxBxhxDxfxhxhxhxhxcxcx'
+		colorflag="--color"
+			export LS_COLORS='no=00:fi=00:di=01;31:ln=01;36:pi=40;33:so=01;35:do=01;35:bd=40;33;01:cd=40;33;01:or=40;31;01:ex=01;32:*.tar=01;31:*.tgz=01;31:*.arj=01;31:*.taz=01;31:*.lzh=01;31:*.zip=01;31:*.z=01;31:*.Z=01;31:*.gz=01;31:*.bz2=01;31:*.deb=01;31:*.rpm=01;31:*.jar=01;31:*.jpg=01;35:*.jpeg=01;35:*.gif=01;35:*.bmp=01;35:*.pbm=01;35:*.pgm=01;35:*.ppm=01;35:*.tga=01;35:*.xbm=01;35:*.xpm=01;35:*.tif=01;35:*.tiff=01;35:*.png=01;35:*.mov=01;35:*.mpg=01;35:*.mpeg=01;35:*.avi=01;35:*.fli=01;35:*.gl=01;35:*.dl=01;35:*.xcf=01;35:*.xwd=01;35:*.ogg=01;35:*.mp3=01;35:*.wav=01;35:'
+		else # macOS `ls`
+				colorflag="-G"
+					export LSCOLORS='BxBxhxDxfxhxhxhxhxcxcx'
 fi
 
 # List all files colorized in long format
@@ -116,41 +125,35 @@ alias gck="git checkout"
 
 # Create a new directory and enter it
 function mkd() {
-    mkdir -p "$@" && cd "$@"
-}
+	    mkdir -p "$@" && cd "$@"
+    }
 
 function md() {
-    mkdir -p "$@" && cd "$@"
-}
+	    mkdir -p "$@" && cd "$@"
+    }
 
 function xtree {
-    find ${1:-.} -print | sed -e 's;[^/]*/;|____;g;s;____|; |;g'
-}
+	    find ${1:-.} -print | sed -e 's;[^/]*/;|____;g;s;____|; |;g'
+    }
 
 # `tre` is a shorthand for `tree` with hidden files and color enabled, ignoring
 # the `.git` directory, listing directories first. The output gets piped into
 # `less` with options to preserve color and line numbers, unless the output is
 # small enough for one screen.
 function tre() {
-	tree -aC -I '.git|node_modules|bower_components' --dirsfirst "$@" | less -FRNX;
-}
+		tree -aC -I '.git|node_modules|bower_components' --dirsfirst "$@" | less -FRNX;
+	}
 
 # Determine size of a file or total size of a directory
 function fs() {
-	if du -b /dev/null > /dev/null 2>&1; then
-		local arg=-sbh;
-	else
-		local arg=-sh;
-	fi
-	if [[ -n "$@" ]]; then
-		du $arg -- "$@";
-	else
-		du $arg .[^.]* ./*;
-	fi;
+		if du -b /dev/null > /dev/null 2>&1; then
+     local arg=-sbh;
+    else
+      local arg=-sh;
+    fi
+    if [[ -n "$@" ]]; then
+      du $arg -- "$@";
+    else
+      du $arg .[^.]* ./*;
+    fi;
 }
-
-alias ide:generate="
-php artisan ide-helper:generate && \
-php artisan ide-helper:meta && \
-php artisan ide-helper:models --nowrite
-"
